5dc25ec46c10e35ec12718a8b47506cf1bd9e446
Abiyoga Hendra Wijaya <abiyogakhusus@gmail.com>
2021-06-11 21:32:13 +0700

    first

---
+++ resources/views/layout/master-layout.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Dashboard</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="{{asset('template/css/font-face.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="{{asset('template/vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
+
+    <!-- template/vendor CSS-->
+    <link href="{{asset('template/vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/wow/animate.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/slick/slick.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('template/vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="{{asset('template/css/theme.css')}}" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <!-- HEADER MOBILE-->
+            @include('layout.header-layout')
+        <!-- END HEADER MOBILE-->
+
+        <!-- MENU SIDEBAR-->
+            @include('layout.sidebar-layout')
+        <!-- END MENU SIDEBAR-->
+
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+               
+            </div>
+            <!-- END MAIN CONTENT-->
+            <!-- END PAGE CONTAINER-->
+        </div>
+
+    </div>
+
+    <!-- Jquery js')}}-->
+    <script src="{{asset('template/vendor/jquery-3.2.1.min.js')}}"></script>
+    <!-- Bootstrap js')}}-->
+    <script src="{{asset('template/vendor/bootstrap-4.1/popper.min.js')}}"></script>
+    <script src="{{asset('template/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
+  
+    <script src="{{asset('template/vendor/slick/slick.min.js')}}">
+    </script>
+    <script src="{{asset('template/vendor/wow/wow.min.js')}}"></script>
+    <script src="{{asset('template/vendor/animsition/animsition.min.js')}}"></script>
+    <script src="{{asset('template/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}">
+    </script>
+    <script src="{{asset('template/vendor/counter-up/jquery.waypoints.min.js')}}"></script>
+    <script src="{{asset('template/vendor/counter-up/jquery.counterup.min.js')}}">
+    </script>
+    <script src="{{asset('template/vendor/circle-progress/circle-progress.min.js')}}"></script>
+    <script src="{{asset('template/vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset('template/vendor/chartjs')}}/Chart.bundle.min.js')}}"></script>
+    <script src="{{asset('template/vendor/select2/select2.min.js')}}">
+    </script>
+
+    <!-- Main js')}}-->
+    <script src="{{asset('template/js/main.js')}}"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [DashboardController::class, 'IndexDashboard'])->name('dashboard-admin');
