8e7d142aaa039d16c797ac04f1f6992a6d5cdb51
abiyogahendra <abiyogakhusus@gmail.com>
2021-06-18 10:49:51 +0700

    F.E Data Mining Ready

--- public/custom/js/landing-page/data-mining.js
+++ public/custom/js/landing-page/data-mining.js
@@ -11,6 +11,51 @@ function IndexDataMining(){
             $('.mother').append(html);
             $('.nav-active').removeClass("active");
             $('.nav-data-mining').addClass("active");
+
+            $('#table-data-mining').DataTable({
+                ajax : {
+                    url : '/data-table-data-talent',
+                    dataSrc : ''
+                  },
+                  "columnDefs": [ 
+                    {"targets": 0,
+                      "data": 0,
+                      "render": function ( data, type, row, meta ) {
+                        return '<div class="row center"><div class="col">'+data+'</div></div>';
+                      }
+                    },
+                    {"targets": 1,
+                      "data": 1,
+                      "render": function ( data, type, row, meta ) {
+                        return '<div class="row center"><div class="col">'+data+'</div></div>';
+                      }
+                    },
+                    {"targets": 2,
+                      "data": 2,
+                      "render": function ( data, type, row, meta ) {
+                        return '<div class="row center"><div class="col">'+data+'</div></div>';
+                      }
+                    },
+                    {"targets": 3,
+                      "data": 3,
+                      "render": function ( data, type, row, meta ) {
+                        return '<div class="row center"><div class="col">'+data+'</div></div>';
+                      }
+                    },
+                    {"targets": 4,
+                      "data": 4,
+                      "render": function ( data, type, row, meta ) {
+                        return '<div class="row center"><div class="col">'+data+'</div></div>';
+                      }
+                    },
+                    {"targets": 5,
+                      "data": 5,
+                      "render": function ( data, type, row, meta ) {
+                        return '<div class="row center"><div class="col center">'+data+'</div></div>';
+                      }
+                    },
+                ],
+            });
         }
     })
 }
--- resources/views/page/data-mining.blade.php
+++ resources/views/page/data-mining.blade.php
@@ -5,13 +5,42 @@
                 <div class="col">
                     <!-- USER DATA-->
                     <div class="user-data">
-                        <h1 class="title-3 m-b-30"><i class="zmdi zmdi-account-calendar"></i>Data Mining Page</h1>
+                        <h1 class="title-3 m-b-30 center"><i class="zmdi zmdi-account-calendar"></i>Data Mining Page</h1>
                         <div class="user-data__footer">
-                            <button class="au-btn au-btn-load">load more</button>
+                            <button class="btn btn-outline-success btn-lg" onclick="#"><i class="fas fa-plus-square"></i> Proses Mining</button>
                         </div>
                     </div>
                     <!-- END USER DATA-->
                 </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <!-- USER DATA-->
+                <div class="user-data">
+                    <h1 class="title-3 m-b-30 center"><i class="zmdi zmdi-account-calendar"></i>Data Talent</h1>
+                    <div class="container">
+                        <div class="table-responsive table--no-card m-b-30">
+                            <table class="table table-borderless table-striped table-earning" id="table-data-mining">
+                                <thead>
+                                    <tr class="center">
+                                        <th>ID Username</th>
+                                        <th>Player Experience</th>
+                                        <th>Skill</th>
+                                        <th>Intelligence</th>
+                                        <th>Attitude</th>
+                                        <th>Turnamen</th>
+                                        <th>Target</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- END USER DATA-->
             </div>
         </div>
     </div>
